f6085cda912780adb1630f4fb9ac77e25767ba62
shayankhannn <shayannkhannn007@gmail.com>
2026-04-04 01:19:23 -0700

    title size changes

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -18,6 +18,17 @@ const EventCard = ({title,image,location,slug,date,time}:Props) => {
         <p className="">{location}</p>
         </div>
         <p className="title">{title}</p>
+      <div className="datetime">
+        <div>
+          <Image src="icons/calendar.svg" width={14} height={14} alt="date" ></Image>
+        <p className="">{date}</p>
+        </div>
+        <div>
+          <Image src="icons/clock.svg" width={14} height={14} alt="time" ></Image>
+        <p className="">{time}</p>
+        </div>
+      </div>
+    
     </Link>
   )
 }
